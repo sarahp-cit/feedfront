2c4046b80efba1ad3737a38d3ade729d7544a66c
sarahp_cit <sarahp@ciandt.com>
2022-07-22 08:19:56 -0300

    Ajuste na classe de testes

--- src/test/java/com/ciandt/feedfront/employee/EmployeeTest.java
+++ src/test/java/com/ciandt/feedfront/employee/EmployeeTest.java
@@ -1,36 +1,53 @@
 package com.ciandt.feedfront.employee;
 
-
 import com.ciandt.feedfront.excecoes.ArquivoException;
 import com.ciandt.feedfront.excecoes.ComprimentoInvalidoException;
 import com.ciandt.feedfront.excecoes.EmailInvalidoException;
 import com.ciandt.feedfront.excecoes.EmployeeNaoEncontradoException;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmployeeTest {
-
     public Employee employee1;
     public Employee employee2;
 
-    @BeforeAll
-    public void initEach() throws ComprimentoInvalidoException {
+    @BeforeEach
+    public void initEach() throws ComprimentoInvalidoException, ArquivoException {
         employee1 = new Employee("Jose", "Silveira", "j.silveira@email.com");
         employee2 = null;
+
+        try {
+            Files.walk(Paths.get("src/main/resources"))
+                    .filter(p -> p.toString().endsWith(".byte"))
+                    .forEach(p -> {
+                        new File(p.toString()).delete();
+                    });
+        } catch (Exception e) {
+
+        }
+
+        try {
+            Employee.salvarEmployee(employee1);
+        } catch (EmailInvalidoException e) {
+            e.getMessage();
+        }
     }
 
     @Test
-    @Order(1)
     public void salvarEmployeeTest() throws ComprimentoInvalidoException, EmailInvalidoException, ArquivoException, EmployeeNaoEncontradoException {
-        employee2 = new Employee("João", "Silveira", "j.silveira@email.com");
+        employee2 = new Employee("João", "Silveira", "joao@email.com");
 
-        Employee.salvarEmployee(employee1);
+        Employee.salvarEmployee(employee2);
 
         Exception emailException = assertThrows(EmailInvalidoException.class, () -> Employee.salvarEmployee(employee2));
 
@@ -41,7 +58,6 @@ public class EmployeeTest {
     }
 
     @Test
-    @Order(2)
     public void listarEmployees() throws ArquivoException {
         List<Employee> employees = Employee.listarEmployees();
 
@@ -50,7 +66,6 @@ public class EmployeeTest {
     }
 
     @Test
-    @Order(3)
     public void buscarEmployee() throws ArquivoException, EmployeeNaoEncontradoException {
         Employee retornoDePesquisa = Employee.buscarEmployee(employee1.getId());
 
@@ -62,7 +77,6 @@ public class EmployeeTest {
     }
 
     @Test
-    @Order(4)
     public void atualizarEmployee() throws ComprimentoInvalidoException, EmployeeNaoEncontradoException, ArquivoException, EmailInvalidoException {
         String sobrenome = "Roberto";
         employee1.setSobrenome(sobrenome);
@@ -77,7 +91,6 @@ public class EmployeeTest {
     }
 
     @Test
-    @Order(7)
     public void apagarEmployee() throws ArquivoException, EmployeeNaoEncontradoException {
         String id = employee1.getId();
         Employee.apagarEmployee(id);
@@ -90,7 +103,6 @@ public class EmployeeTest {
     }
 
     @Test
-    @Order(5)
     public void nomeDeveTerComprimentoMaiorQueDois() {
         Exception comprimentoInvalidoException = assertThrows(ComprimentoInvalidoException.class, () ->
                 employee1 = new Employee("Ze", "Juvenil", "z.juvenil@ciandt.com")
@@ -102,7 +114,6 @@ public class EmployeeTest {
     }
 
     @Test
-    @Order(6)
     public void sobrenomeDeveTerComprimentoMaiorQueDois() {
         Exception comprimentoInvalidoException = assertThrows(ComprimentoInvalidoException.class, () ->
                 employee1 = new Employee("Joao", "ao", "z.juvenil@ciandt.com")
