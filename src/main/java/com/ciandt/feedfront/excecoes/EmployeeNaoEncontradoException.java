9846bb6d3225649040424947d4ba15f827403572
sarahp_cit <sarahp@ciandt.com>
2022-07-25 17:30:31 -0300

    Remoção exception não utilizada

--- src/main/java/com/ciandt/feedfront/excecoes/EmployeeNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.ciandt.feedfront.excecoes;
-
-public class EmployeeNaoEncontradoException extends BusinessException {
-    public EmployeeNaoEncontradoException(String message) {
-        super(message);
-    }
-}
